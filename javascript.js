75201d2244c72a49b5adcdf33a49a928346d28a0
Meghan Offredo <meghan.offredo@gmail.com>
2021-11-10 16:24:12 +0400

    add change function to button after 1st click

--- javascript.js
+++ javascript.js
@@ -10,15 +10,25 @@ const restaurantArray = [
     "images/tap-house.png"
 ];
 
-// Connect to HTML
+// Access img and button elements
 const restaurant = document.querySelector("img");
 const button = document.querySelector("button");
 
 // Generate new selection on click
 button.addEventListener("click", () => generateRandomRestaurant(restaurantArray));
+button.addEventListener("click", () => change());
 
-// Function to display random restaurant
+let arrayEnd = restaurantArray.length;
+
+// Function to get random restaurant
 function generateRandomRestaurant(array){
-    let randomNum = Math.floor(Math.random() * array.length);
-    restaurant.setAttribute("src", array[randomNum]);
+        let randomNum = Math.floor(Math.random() * array.length);
+        restaurant.setAttribute("src", array[randomNum]);
 }
+
+
+// Function to change button text after first click
+function change(){
+    button.innerHTML = "Choose Another";
+}
+
